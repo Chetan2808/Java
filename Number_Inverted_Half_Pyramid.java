fe8edf00d76d8984f8c595f71311204e1c31968d
Chetan Gandhi <69512667+Chetan2808@users.noreply.github.com>
2021-12-21 19:35:55 +0530

    Add files via upload
    Basic Pattern Questions

---
+++ Number_Inverted_Half_Pyramid.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Number_Inverted_Half_Pyramid {
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter the number of rows for pattern: ");
+        int n = input.nextInt();
+        for(int  i = n ; i != 0 ; i--){
+            for(int j = 1 ; j <= i ; j++){
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+    }
+}
