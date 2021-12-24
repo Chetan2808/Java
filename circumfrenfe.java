9ac7faeba3a28e9dcc1b4b9d339a5c7eb43c2504
Chetan Gandhi <69512667+Chetan2808@users.noreply.github.com>
2021-12-25 01:28:19 +0530

    It returns the circumfrence of circle.

---
+++ circumfrenfe.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class circumfrenfe {
+    public double circumfrence(double radius){
+        return (6.28) * radius;
+    }
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        double radius = input.nextDouble();
+        circumfrenfe pack = new circumfrenfe();
+        System.out.println(pack.circumfrence(radius));
+    }
+}
