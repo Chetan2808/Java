c62fe83338f0ab107a28dd91169a3fb1a87a2f46
Chetan Gandhi <69512667+Chetan2808@users.noreply.github.com>
2021-12-25 01:48:21 +0530

    it results the exponential value of two number.

---
+++ exponential.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class exponential {
+    public double expo(double x , double n){
+        if(x <= 0){
+            System.out.println("Entered number can't be 0 or negative");
+            return  0 ;
+        }
+        else {
+            return Math.pow(x,n);
+        }
+
+    }
+    public static void main(String[] args){
+        exponential pack = new exponential();
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter the number: ");
+        double x = input.nextDouble();
+        System.out.print("Enter the power : ");
+        double n = input.nextDouble();
+        System.out.print("The result is: ");
+        System.out.print(pack.expo(x,n));
+    }
+}
